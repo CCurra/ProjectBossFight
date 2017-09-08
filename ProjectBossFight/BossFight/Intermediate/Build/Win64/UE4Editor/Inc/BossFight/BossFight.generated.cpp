aed107d43d5ee7ebf8d507470f62325fe747d088
Richland Gaming <ccurra@dcccd.edu>
2017-09-08 11:49:16 -0500

    New

---
+++ ProjectBossFight/BossFight/Intermediate/Build/Win64/UE4Editor/Inc/BossFight/BossFight.generated.cpp
@@ -0,0 +1,150 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "GeneratedCppIncludes.h"
+#include "BossFight.generated.dep.h"
+PRAGMA_DISABLE_OPTIMIZATION
+#ifdef _MSC_VER
+#pragma warning (push)
+#pragma warning (disable : 4883)
+#endif
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCode1BossFight() {}
+#if USE_COMPILED_IN_NATIVES
+// Cross Module References
+	ENGINE_API class UClass* Z_Construct_UClass_ACharacter();
+	ENGINE_API class UClass* Z_Construct_UClass_UCameraComponent_NoRegister();
+	ENGINE_API class UClass* Z_Construct_UClass_USpringArmComponent_NoRegister();
+	ENGINE_API class UClass* Z_Construct_UClass_AGameModeBase();
+
+	BOSSFIGHT_API class UClass* Z_Construct_UClass_ABossFightCharacter_NoRegister();
+	BOSSFIGHT_API class UClass* Z_Construct_UClass_ABossFightCharacter();
+	BOSSFIGHT_API class UClass* Z_Construct_UClass_ABossFightGameMode_NoRegister();
+	BOSSFIGHT_API class UClass* Z_Construct_UClass_ABossFightGameMode();
+	BOSSFIGHT_API class UPackage* Z_Construct_UPackage__Script_BossFight();
+	void ABossFightCharacter::StaticRegisterNativesABossFightCharacter()
+	{
+	}
+	UClass* Z_Construct_UClass_ABossFightCharacter_NoRegister()
+	{
+		return ABossFightCharacter::StaticClass();
+	}
+	UClass* Z_Construct_UClass_ABossFightCharacter()
+	{
+		static UClass* OuterClass = NULL;
+		if (!OuterClass)
+		{
+			Z_Construct_UClass_ACharacter();
+			Z_Construct_UPackage__Script_BossFight();
+			OuterClass = ABossFightCharacter::StaticClass();
+			if (!(OuterClass->ClassFlags & CLASS_Constructed))
+			{
+				UObjectForceRegistration(OuterClass);
+				OuterClass->ClassFlags |= 0x20800080;
+
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+				UProperty* NewProp_BaseLookUpRate = new(EC_InternalUseOnlyConstructor, OuterClass, TEXT("BaseLookUpRate"), RF_Public|RF_Transient|RF_MarkAsNative) UFloatProperty(CPP_PROPERTY_BASE(BaseLookUpRate, ABossFightCharacter), 0x0010000000020015);
+				UProperty* NewProp_BaseTurnRate = new(EC_InternalUseOnlyConstructor, OuterClass, TEXT("BaseTurnRate"), RF_Public|RF_Transient|RF_MarkAsNative) UFloatProperty(CPP_PROPERTY_BASE(BaseTurnRate, ABossFightCharacter), 0x0010000000020015);
+				UProperty* NewProp_FollowCamera = new(EC_InternalUseOnlyConstructor, OuterClass, TEXT("FollowCamera"), RF_Public|RF_Transient|RF_MarkAsNative) UObjectProperty(CPP_PROPERTY_BASE(FollowCamera, ABossFightCharacter), 0x00400000000a001d, Z_Construct_UClass_UCameraComponent_NoRegister());
+				UProperty* NewProp_CameraBoom = new(EC_InternalUseOnlyConstructor, OuterClass, TEXT("CameraBoom"), RF_Public|RF_Transient|RF_MarkAsNative) UObjectProperty(CPP_PROPERTY_BASE(CameraBoom, ABossFightCharacter), 0x00400000000a001d, Z_Construct_UClass_USpringArmComponent_NoRegister());
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+				OuterClass->ClassConfigName = FName(TEXT("Game"));
+				static TCppClassTypeInfo<TCppClassTypeTraits<ABossFightCharacter> > StaticCppClassTypeInfo;
+				OuterClass->SetCppTypeInfo(&StaticCppClassTypeInfo);
+				OuterClass->StaticLink();
+#if WITH_METADATA
+				UMetaData* MetaData = OuterClass->GetOutermost()->GetMetaData();
+				MetaData->SetValue(OuterClass, TEXT("HideCategories"), TEXT("Navigation"));
+				MetaData->SetValue(OuterClass, TEXT("IncludePath"), TEXT("BossFightCharacter.h"));
+				MetaData->SetValue(OuterClass, TEXT("ModuleRelativePath"), TEXT("BossFightCharacter.h"));
+				MetaData->SetValue(NewProp_BaseLookUpRate, TEXT("Category"), TEXT("Camera"));
+				MetaData->SetValue(NewProp_BaseLookUpRate, TEXT("ModuleRelativePath"), TEXT("BossFightCharacter.h"));
+				MetaData->SetValue(NewProp_BaseLookUpRate, TEXT("ToolTip"), TEXT("Base look up/down rate, in deg/sec. Other scaling may affect final rate."));
+				MetaData->SetValue(NewProp_BaseTurnRate, TEXT("Category"), TEXT("Camera"));
+				MetaData->SetValue(NewProp_BaseTurnRate, TEXT("ModuleRelativePath"), TEXT("BossFightCharacter.h"));
+				MetaData->SetValue(NewProp_BaseTurnRate, TEXT("ToolTip"), TEXT("Base turn rate, in deg/sec. Other scaling may affect final turn rate."));
+				MetaData->SetValue(NewProp_FollowCamera, TEXT("AllowPrivateAccess"), TEXT("true"));
+				MetaData->SetValue(NewProp_FollowCamera, TEXT("Category"), TEXT("Camera"));
+				MetaData->SetValue(NewProp_FollowCamera, TEXT("EditInline"), TEXT("true"));
+				MetaData->SetValue(NewProp_FollowCamera, TEXT("ModuleRelativePath"), TEXT("BossFightCharacter.h"));
+				MetaData->SetValue(NewProp_FollowCamera, TEXT("ToolTip"), TEXT("Follow camera"));
+				MetaData->SetValue(NewProp_CameraBoom, TEXT("AllowPrivateAccess"), TEXT("true"));
+				MetaData->SetValue(NewProp_CameraBoom, TEXT("Category"), TEXT("Camera"));
+				MetaData->SetValue(NewProp_CameraBoom, TEXT("EditInline"), TEXT("true"));
+				MetaData->SetValue(NewProp_CameraBoom, TEXT("ModuleRelativePath"), TEXT("BossFightCharacter.h"));
+				MetaData->SetValue(NewProp_CameraBoom, TEXT("ToolTip"), TEXT("Camera boom positioning the camera behind the character"));
+#endif
+			}
+		}
+		check(OuterClass->GetClass());
+		return OuterClass;
+	}
+	IMPLEMENT_CLASS(ABossFightCharacter, 3382380865);
+	static FCompiledInDefer Z_CompiledInDefer_UClass_ABossFightCharacter(Z_Construct_UClass_ABossFightCharacter, &ABossFightCharacter::StaticClass, TEXT("/Script/BossFight"), TEXT("ABossFightCharacter"), false, nullptr, nullptr, nullptr);
+	DEFINE_VTABLE_PTR_HELPER_CTOR(ABossFightCharacter);
+	void ABossFightGameMode::StaticRegisterNativesABossFightGameMode()
+	{
+	}
+	UClass* Z_Construct_UClass_ABossFightGameMode_NoRegister()
+	{
+		return ABossFightGameMode::StaticClass();
+	}
+	UClass* Z_Construct_UClass_ABossFightGameMode()
+	{
+		static UClass* OuterClass = NULL;
+		if (!OuterClass)
+		{
+			Z_Construct_UClass_AGameModeBase();
+			Z_Construct_UPackage__Script_BossFight();
+			OuterClass = ABossFightGameMode::StaticClass();
+			if (!(OuterClass->ClassFlags & CLASS_Constructed))
+			{
+				UObjectForceRegistration(OuterClass);
+				OuterClass->ClassFlags |= 0x20880288;
+
+
+				static TCppClassTypeInfo<TCppClassTypeTraits<ABossFightGameMode> > StaticCppClassTypeInfo;
+				OuterClass->SetCppTypeInfo(&StaticCppClassTypeInfo);
+				OuterClass->StaticLink();
+#if WITH_METADATA
+				UMetaData* MetaData = OuterClass->GetOutermost()->GetMetaData();
+				MetaData->SetValue(OuterClass, TEXT("HideCategories"), TEXT("Info Rendering MovementReplication Replication Actor Input Movement Collision Rendering Utilities|Transformation"));
+				MetaData->SetValue(OuterClass, TEXT("IncludePath"), TEXT("BossFightGameMode.h"));
+				MetaData->SetValue(OuterClass, TEXT("ModuleRelativePath"), TEXT("BossFightGameMode.h"));
+				MetaData->SetValue(OuterClass, TEXT("ShowCategories"), TEXT("Input|MouseInput Input|TouchInput"));
+#endif
+			}
+		}
+		check(OuterClass->GetClass());
+		return OuterClass;
+	}
+	IMPLEMENT_CLASS(ABossFightGameMode, 2205408425);
+	static FCompiledInDefer Z_CompiledInDefer_UClass_ABossFightGameMode(Z_Construct_UClass_ABossFightGameMode, &ABossFightGameMode::StaticClass, TEXT("/Script/BossFight"), TEXT("ABossFightGameMode"), false, nullptr, nullptr, nullptr);
+	DEFINE_VTABLE_PTR_HELPER_CTOR(ABossFightGameMode);
+	UPackage* Z_Construct_UPackage__Script_BossFight()
+	{
+		static UPackage* ReturnPackage = nullptr;
+		if (!ReturnPackage)
+		{
+			ReturnPackage = CastChecked<UPackage>(StaticFindObjectFast(UPackage::StaticClass(), nullptr, FName(TEXT("/Script/BossFight")), false, false));
+			ReturnPackage->SetPackageFlags(PKG_CompiledIn | 0x00000000);
+			FGuid Guid;
+			Guid.A = 0x1FC33E4E;
+			Guid.B = 0x2BCC832C;
+			Guid.C = 0x00000000;
+			Guid.D = 0x00000000;
+			ReturnPackage->SetGuid(Guid);
+
+		}
+		return ReturnPackage;
+	}
+#endif
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
+PRAGMA_ENABLE_OPTIMIZATION
---
+++ ProjectBossFight/BossFight/Intermediate/Build/Win64/UE4Editor/Inc/BossFight/BossFight.generated.dep.h
@@ -0,0 +1,13 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "GeneratedCppIncludes.h"
+#ifndef BOSSFIGHT_BossFightCharacter_generated_h
+	#include "BossFightCharacter.h"
+#endif
+#ifndef BOSSFIGHT_BossFightGameMode_generated_h
+	#include "BossFightGameMode.h"
+#endif
